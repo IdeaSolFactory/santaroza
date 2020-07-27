26fea3cfd01284be37cbeb890dcbfc4dfbd7fe6e
gabor-isf <tamasi@isf.hu>
2020-07-27 14:07:59 +0200

    reset counter

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@
     });         
 
     // countdown timer js 
-    $('.coundown_res').countdown('2020/07/21', function (event) {
+    $('.coundown_res').countdown('2020/07/30', function (event) {
         var $this = $(this);
         $('#day').html(event.strftime('<span>%D</span>'));
         $('#hour').html(event.strftime('<span>%H</span>'));
