aa8e98b536821ba39444940e418df95c35d42fb4
gabor-isf <tamasi@isf.hu>
2020-08-03 10:16:03 +0200

    counter updated

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@
     });         
 
     // countdown timer js 
-    $('.coundown_res').countdown('2020/08/02 23:59:59', function (event) {
+    $('.coundown_res').countdown('2020/08/20 00:00:00', function (event) {
         var $this = $(this);
         $('#day').html(event.strftime('<span>%D</span>'));
         $('#hour').html(event.strftime('<span>%H</span>'));
